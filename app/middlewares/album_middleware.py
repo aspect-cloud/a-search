748fdda260a5313b5a0495f042894ee3cbd6590a
aspect-cloud <aspectcloudoff@gmail.com>
2025-06-22 03:45:46 +0300

    pyatiletku za 5 dnya

--- app/middlewares/album_middleware.py
+++ app/middlewares/album_middleware.py
@@ -1,13 +1,11 @@
 import asyncio
-from typing import Callable, Dict, Any, Awaitable
+from typing import Any, Awaitable, Callable, Dict
 
 from aiogram import BaseMiddleware
 from aiogram.types import Message
 
 
 class AlbumMiddleware(BaseMiddleware):
-    """This middleware is for collecting updates from media groups."""
-
     album_data: dict = {}
 
     def __init__(self, latency: int | float = 0.1):
@@ -19,18 +17,27 @@ class AlbumMiddleware(BaseMiddleware):
         event: Message,
         data: Dict[str, Any],
     ) -> Any:
-        if not event.media_group_id:
+        # This middleware should only handle messages with photos.
+        # For any other message type, it should be a passthrough.
+        if not event.photo:
             return await handler(event, data)
 
+        # If the message is a single photo (not in a media group).
+        if not event.media_group_id:
+            data["album"] = [event]
+            return await handler(event, data)
+
+        # If the message is part of a media group.
         try:
             self.album_data[event.media_group_id].append(event)
         except KeyError:
             self.album_data[event.media_group_id] = [event]
             await asyncio.sleep(self.latency)
 
-            data["_is_last"] = True
-            data["album"] = self.album_data[event.media_group_id]
-            await handler(event, data)
+            messages = self.album_data.pop(event.media_group_id)
+            data["album"] = messages
+            return await handler(event, data)
 
-        if data.get("_is_last"):
-            del self.album_data[event.media_group_id]
+        # If the message is part of an album but not the first one,
+        # stop processing it further down the chain.
+        return
